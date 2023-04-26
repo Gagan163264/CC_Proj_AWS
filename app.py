22f9ec81d765568325e4d2e8b72bd8d0e583d3f0
Gagan <42744771+Gagan163264@users.noreply.github.com>
2023-04-27 02:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import re
 app = Flask(__name__)
 
 app.secret_key = 'pawtropica'
+#fill keys
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'pes1ug20cs182'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pawtropica'
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
  
